e846334349bb61f91ec02f8c3173a192ff506eb1
inok94 <rodiknikolaev@yandex.ru>
2017-02-08 23:26:45 +0300

    test push

--- views/category/index.php
+++ views/category/index.php
@@ -128,6 +128,7 @@ $this->title = 'My Yii Application';
                                     <p><?php $hit->name ?></p>
                                     <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                 </div>
+                                
                                <!-- <div class="product-overlay">
                                     <div class="overlay-content">
                                         <h2><?php /*$hit->price */?></h2>

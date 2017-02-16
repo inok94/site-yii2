1dd68db56465fe82df487c666208e8fe5736f3ad
inok94 <rodiknikolaev@yadndex.ru>
2017-02-17 00:00:27 +0300

    Пагиная страниц, выввод 3-х товаров на странице. Дискрипшен, ключевые слова...

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -15,5 +15,16 @@ use yii\web\Controller;
  */
 class AppController extends Controller
 {
+    /**
+     * @param null $title
+     * @param null $keywords
+     * @param null $description
+     */
+    protected  function setMeta($title = null, $keywords = null, $description = null)
+    {
+        $this->view->title = $title;
+        $this->view->registerMetaTag(['name' => 'keywords', 'content'=> "$keywords"]);
+        $this->view->registerMetaTag(['name' => 'description', 'content'=> "$description"]);
+    }
 
 }
--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -4,12 +4,33 @@ namespace app\controllers;
 
 use app\models\Category;
 use app\models\Product;
+use yii\data\Pagination;
 use Yii;
 class CategoryController extends AppController
 {
     public function actionIndex()
     {   $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        $this->setMeta('E-SHOPPER');
         //debug($hits);
         return $this->render('index', compact(['hits']));
+    }
+    public function actionView($id)
+    {
+        $id = Yii::$app->request->get('id');
+        //debug($id);
+        //$products = Product::find()->where(['category_id'  => $id])->all();
+        $query = Product::find()->where(['category_id'  => $id]);
+        //Пагинация страниц с лемитом 3 товара.
+        $pages = new Pagination(['totalCount' => $query->count(),
+                                    'pageSize' => 3,
+                                    'forcePageParam' => false,
+                                    'pageSizeParam' => false,
+        ]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        $category = Category::findOne($id);
+        $this->setMeta('E-SHOPPER | ' .$category->name, $category->keywords, $category->description);
+        //debug($products);
+        return $this->render('view', compact('products', 'pages', 'category'));
     }
 }
